769f65718cc15adc5a289403479d6f7e403b3f1e
zac4312 <sushi.ninja315@gmail.com>
2025-12-15 22:52:13 +0800

    initial commit

---
+++ guessing-game/src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+use rand::Rng;
+
+
+fn main() {
+ 
+    let mut choice = String::new();
+    io::stdin().read_line(&mut choice).expect("wrong input");
+
+    while choice.trim() == "y" {
+
+        println!("-------------------------------------");
+        println!("Guess a number from 1-5!");
+
+        let num = rand::thread_rng().gen_range(1..=5);
+        let str_num = num.to_string(); 
+
+        println!("Enter a guess:");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+                   .expect("Wrong guess");
+
+        if guess.trim() == str_num { 
+            println!("Corrrect!");
+            println!("You guess: {}", guess.trim());
+            println!("Answer: {str_num}");
+        } else { println!("Incorroce!"); 
+                 println!("The number was: {str_num}"); }
+
+        println!("Again? (y/n): ");
+
+        choice.clear();
+        io::stdin().read_line(&mut choice)
+                   .expect("wrong input");
+                    
+    }
+
+    println!("end");
+}
