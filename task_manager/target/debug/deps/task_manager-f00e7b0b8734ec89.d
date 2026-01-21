47eeb1f2070ca86cc3fb4f7881281575d0e300fb
zac4312 <sushi.ninja315@gmail.com>
2026-01-21 12:44:44 +0800

    phone

---
+++ task_manager/target/debug/deps/task_manager-f00e7b0b8734ec89.d
@@ -0,0 +1,6 @@
+/data/data/com.termux/files/home/PROJ/rust-learning/task_manager/target/debug/deps/task_manager-f00e7b0b8734ec89.d: src/main.rs src/controller.rs
+
+/data/data/com.termux/files/home/PROJ/rust-learning/task_manager/target/debug/deps/task_manager-f00e7b0b8734ec89: src/main.rs src/controller.rs
+
+src/main.rs:
+src/controller.rs:
